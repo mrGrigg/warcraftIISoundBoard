bdad18950742eecfdc091d3d71a3edc552252ff5
mrGrigg <joe.grigg@gmail.com>
2013-02-27 15:42:59 -0700

    Update public/app/component_ui/Unit.js

--- public/app/component_ui/Unit.js
+++ public/app/component_ui/Unit.js
@@ -251,7 +251,7 @@ define(function(require) {
         };
 
         this.newSound = function(fileName) {
-            var soundUrl = '/sounds/human/' + fileName
+            var soundUrl = 'https://raw.github.com/mrGrigg/warcraftIISoundBoard/master/public/sounds/human/' + fileName
                 , newSound = {
                     volume: 0.5
                     , onend: component.bindClickToPlay
